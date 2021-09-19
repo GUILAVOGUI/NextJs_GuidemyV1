c9f4d8b3118077a6c364489362b851720d8d1c41
GUILAVOGUI <sayonguilavogui518@yahoo.com>
2021-09-20 03:13:09 +0800

    frontEnd-backEnd

--- sever/controllers/auth.js
+++ sever/controllers/auth.js
@@ -1,7 +1,47 @@
 
+import User from "../models/user"
+import { hashPassword, comparePassword } from "../utils/auth"
 
-export const register = (req, res) => {
-    // console.log(req.body);
-    res.json('register user respond from controller');
+
+
+export const register = async (req, res) => {
+
+    try {
+        const { name, email, password } = req.body
+
+        // Validation
+        if (!name) {
+            return res.status(400).send("Name is required")
+        }
+        if (!password || password.lenght < 6) {
+            return res.status(400).send("Possword is required and Should be minimu 6 characters long")
+        }
+        let userExit = await User.findOne({ email }).exec();
+        if (userExit) {
+            return res.status(400).send("This email is taken")
+
+        }
+
+        //hashing the password
+        const hashedPassword = await hashPassword(password)
+
+        //registring the user
+        const user = await new User({
+            name, email, password: hashPassword,
+
+        });
+        await user.save();
+        console.log("saved user", user);
+
+        return res.json({ ok: true })
+
+    } catch (error) {
+        console.log(err);
+        return res.status(400).send("Erro! try again")
+
+    }
+
+
+
 
 }
