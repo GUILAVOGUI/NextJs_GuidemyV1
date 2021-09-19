bf1ae219ec84262abda4e8f6c6b3be40fa39cba8
GUILAVOGUI <sayonguilavogui518@yahoo.com>
2021-09-20 01:30:31 +0800

    first commit

---
+++ client/ReadMe.txt
@@ -0,0 +1,10 @@
+
+
+
+==================
+Register.js  :
+    Collect User's name , email, and password to Register
+    then send to backend to save in database
+
+
+====================
---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+
+    return (
+        <>
+            <h1 className="jumbotron text-center bg-primary square" > Online Education Market Place </h1>
+            <p> from next js..</p>
+        </>
+    )
+}
+
+export default Index;
